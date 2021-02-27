8ec28be843358ad64e1055149b35c6e7f44ab7bf
Jonathan <jonathan@carter.games>
2021-02-27 14:55:26 +0000

    Lots of work on level scene

--- Assets/Scripts/Level Sequences/PostIntroSequence.cs
+++ Assets/Scripts/Level Sequences/PostIntroSequence.cs
@@ -9,9 +9,29 @@ namespace TotallyNotEvil.Sequences
     {
         [Header("*** Not Base Class ***")]
         [SerializeField] private PlayerController player;
+        [SerializeField] private Animator ghostMachine;
+        [SerializeField] private Sprite ghost;
+
+        private void Awake()
+        {
+            //player.am.SetActive(false);
+        }
+
 
         public override void Function()
         {
+            StartCoroutine(FunctionCO());
+        }
+
+
+        private IEnumerator FunctionCO()
+        {
+            player.am.transform.position = ghostMachine.transform.position;
+            ghostMachine.SetTrigger("MakeGhost");
+            yield return new WaitForSeconds(3f);
+            ghostMachine.enabled = false;
+            ghostMachine.gameObject.GetComponent<SpriteRenderer>().sprite = ghost;
+            player.am.SetActive(true);
             player.am.GetComponent<IDamagable>().CanTakeDamage = true;
         }
     }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -85,7 +85,6 @@ namespace TotallyNotEvil
 
             if (startAsOrb)
             {
-                orb.SetActive(true);
                 am = orb;
             }
 
